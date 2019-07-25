149d7379b7e09835e3ace970032dbffbceb3fbf2
unknown <jakeoneijk@gmail.com>
2019-07-26 08:54:27 +0900

    read exel files

--- src/components/photo/photo.js
+++ src/components/photo/photo.js
@@ -1,12 +1,36 @@
-import React, {Component} from 'react';
+import React, {Component,useEffect} from 'react';
 
 class Photo extends Component {
+  state = {
+    files : null,
+    timestampInfoFiles:[],
+    photos: {}
+  }
 
+  extractTimestampInfoFilesFromfiles(){
+    for (var i in this.state.files){
+      if(this.state.files[i].type == "application/vnd.ms-excel"){
+        this.state.timestampInfoFiles.push(this.state.files[i])
+      }
+    }
+    console.log(this.state.timestampInfoFiles)
+  }
+
+  componentDidMount(){
+
+    const photoDir = document.getElementById("photoDir");
+
+    photoDir.addEventListener('change',(e)=>{
+      this.setState({files: photoDir.files}); 
+      this.extractTimestampInfoFilesFromfiles()
+    })
+
+  }
 
   render(){
     return (
       <div id="photo">
-          Photo
+          <input type="file" id="photoDir" webkitdirectory="true"/>
       </div>
     )
   }
