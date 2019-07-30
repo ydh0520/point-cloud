47653cc75410b8ec3cca127d50d6b4ac7198947e
unknown <jakeoneijk@gmail.com>
2019-07-31 08:30:12 +0900

    data processing in App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,119 @@ import './App.css';
 import CloudPoint from "./components/cloudpoint/cloudPoint"
 import Photo from "./components/photo/photo"
 
+
+
 export default class App extends Component{
+
+    state = {
+    files : null,
+    timestampInfo:{},
+    imgFiles:{},
+    binFiles:[],
+    binFilesNames:[],
+    currentIndexOfBin:0
+    }
+
+   readFile(file){
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+
+      reader.onload = () => {
+        resolve(reader.result);
+      }
+
+      reader.onerror = reject;
+
+      reader.readAsText(file)
+    })
+  }
+
+  readBinFile(file){
+    return new Promise((resolve, reject) => {
+      let binReader = new FileReader();
+
+      binReader.onload = () => {
+        resolve(binReader.result);
+      }
+
+      binReader.onerror = reject;
+
+      binReader.readAsArrayBuffer(file)
+      
+    })
+  }
+
+  async readFileController(){
+    for (var i in this.state.files){
+          if(this.state.files[i].type == "application/vnd.ms-excel"){
+            try{
+              this.state.timestampInfo[this.state.files[i].name] = (await this.readFile(this.state.files[i])).split(/\n|,/)
+            } catch(err) {
+              console.log(err)
+            }
+    
+          }else if(this.state.files[i].type == "image/png"){
+    
+            if(this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]] != undefined){
+              this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]].push(this.state.files[i]);
+            }else{
+              this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]] =[];
+              this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]].push(this.state.files[i]);
+            }
+    
+          }else if(this.state.files[i].type == "application/octet-stream"){
+              this.state.binFilesNames.push(this.state.files[i].name)
+              this.state.binFiles.push(new Float32Array(await this.readBinFile(this.state.files[i])))
+          }
+        }
+
+        console.log(this.state)
+  }
+
+  componentDidMount(){
+
+    const dir = document.getElementById("directory");
+
+    dir.addEventListener('change',(e)=>{
+      this.setState({files: dir.files}); 
+      this.readFileController();
+      
+    })
+
+    document.getElementById("Previous").addEventListener('click',(e) => {
+      if(this.state.currentIndexOfBin <= 0){
+        alert('첫번째 입니다')
+      }else{
+        this.setState(
+          ({currentIndexOfBin}) => ({
+            currentIndexOfBin: currentIndexOfBin - 1
+          })
+        );
+      }
+    })
+
+    document.getElementById("Next").addEventListener('click',(e) => {
+      if(this.state.currentIndexOfBin == this.state.binFiles.length){
+        alert('마지막 입니다')
+      }else{
+        this.setState(
+          ({currentIndexOfBin}) => ({
+            currentIndexOfBin: currentIndexOfBin + 1
+          })
+        );
+      }
+    })
+
+  }
+
    render(){
     return (
       <div id="main">
-        <CloudPoint/>
-        <Photo/>
+        <input type="file" id="directory" webkitdirectory="true"/>
+        <button id="Previous">Previous</button>
+        <button id="Next">Next</button>
+        <CloudPoint binFiles = {this.state.binFiles} index = {this.state.currentIndexOfBin}/>
+        <Photo index = {this.state.currentIndexOfBin} binFilesNames = {this.state.binFilesNames}/>
         
       </div>
     )
--- src/components/photo/photo.js
+++ src/components/photo/photo.js
@@ -3,115 +3,105 @@ import { resolve } from 'dns';
 import { reject, delay } from 'q';
 
 class Photo extends Component {
-  state = {
-    files : null,
-    timestampInfo:{},
-    imgFiles:{},
-    photosRelatedToBin: {}
-  }
+  // state = {
+  //   files : null,
+  //   timestampInfo:{},
+  //   imgFiles:{},
+  //   photosRelatedToBin: {}
+  // }
 
-  readFile(file){
-    return new Promise((resolve, reject) => {
-      let reader = new FileReader();
+ 
 
-      reader.onload = () => {
-        resolve(reader.result);
-      }
+  // findImgsByTimeStamp(timestampOfBi,binName){
+  //    for (var key in this.state.timestampInfo){
+  //     if(key.includes('cameras')){
 
-      reader.onerror = reject;
+  //         var imgName = "";
+  //         var differenceBetweenTimestamps = Infinity;
 
-      reader.readAsText(file)
-    })
-  }
+  //         for(var i = 2; i < this.state.timestampInfo[key].length; i+= 2){
+  //           const diffTemp = Math.abs(this.state.timestampInfo[key][i] - timestampOfBi)
+  //           if(diffTemp <= differenceBetweenTimestamps){
+  //             differenceBetweenTimestamps = diffTemp;
+  //             imgName = this.state.timestampInfo[key][i + 1]
+  //           }else{
+  //             break;
+  //           }
+  //         }
+  //         this.state.photosRelatedToBin[binName][key.substring(7,9) +"/"+ imgName] = null;
+  //         };
+  //   }
+  // }
 
-  findImgsByTimeStamp(timestampOfBi,binName){
-     for (var key in this.state.timestampInfo){
-      if(key.includes('cameras')){
+  // findImgNameRelatedToBin(){
+  //   for(var i = 2; i < this.state.timestampInfo['OUSTER0.csv'].length; i+=2){
+  //     this.state.photosRelatedToBin[this.state.timestampInfo['OUSTER0.csv'][i+1]] = {};
+  //     this.findImgsByTimeStamp(this.state.timestampInfo['OUSTER0.csv'][i],this.state.timestampInfo['OUSTER0.csv'][i+1])
+  //   }
+  // }
 
-          var imgName = "";
-          var differenceBetweenTimestamps = Infinity;
+  // imgFilesToPhotosRelatedToBin(){
+  //   for (var binfileName in this.state.photosRelatedToBin){
+  //     for(var nameOfImg in this.state.photosRelatedToBin[binfileName]){
+  //       const folderName = nameOfImg.split('/')[0]
+  //       const imgName = nameOfImg.split('/')[1]
+  //       for (var i in this.state.imgFiles[folderName]){
+  //         if(this.state.imgFiles[folderName][i].name == imgName+".png"){
+  //           this.state.photosRelatedToBin[binfileName][nameOfImg] = this.state.imgFiles[folderName][i];
+  //           break;
+  //         }
+  //       }
+  //     }
+  //   }
 
-          for(var i = 2; i < this.state.timestampInfo[key].length; i+= 2){
-            const diffTemp = Math.abs(this.state.timestampInfo[key][i] - timestampOfBi)
-            if(diffTemp <= differenceBetweenTimestamps){
-              differenceBetweenTimestamps = diffTemp;
-              imgName = this.state.timestampInfo[key][i + 1]
-            }else{
-              break;
-            }
-          }
-          this.state.photosRelatedToBin[binName][key.substring(7,9) +"/"+ imgName] = null;
-          };
-    }
-  }
+  // }
 
-  findImgNameRelatedToBin(){
-    for(var i = 2; i < this.state.timestampInfo['OUSTER0.csv'].length; i+=2){
-      this.state.photosRelatedToBin[this.state.timestampInfo['OUSTER0.csv'][i+1]] = {};
-      this.findImgsByTimeStamp(this.state.timestampInfo['OUSTER0.csv'][i],this.state.timestampInfo['OUSTER0.csv'][i+1])
-    }
-  }
+  // async controller(){
 
-  imgFilesToPhotosRelatedToBin(){
-    for (var binfileName in this.state.photosRelatedToBin){
-      for(var nameOfImg in this.state.photosRelatedToBin[binfileName]){
-        const folderName = nameOfImg.split('/')[0]
-        const imgName = nameOfImg.split('/')[1]
-        for (var i in this.state.imgFiles[folderName]){
-          if(this.state.imgFiles[folderName][i].name == imgName+".png"){
-            this.state.photosRelatedToBin[binfileName][nameOfImg] = this.state.imgFiles[folderName][i];
-            break;
-          }
-        }
-      }
-    }
+  //   for (var i in this.state.files){
+  //     if(this.state.files[i].type == "application/vnd.ms-excel"){
 
-  }
+  //       try{
+  //         this.state.timestampInfo[this.state.files[i].name] = (await this.readFile(this.state.files[i])).split(/\n|,/)
+  //       } catch(err) {
+  //         console.log(err)
+  //       }
 
-  async controller(){
+  //     }else if(this.state.files[i].type == "image/png"){
 
-    for (var i in this.state.files){
-      if(this.state.files[i].type == "application/vnd.ms-excel"){
+  //       if(this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]] != undefined){
+  //         this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]].push(this.state.files[i]);
+  //       }else{
+  //         this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]] =[];
+  //         this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]].push(this.state.files[i]);
+  //       }
 
-        try{
-          this.state.timestampInfo[this.state.files[i].name] = (await this.readFile(this.state.files[i])).split(/\n|,/)
-        } catch(err) {
-          console.log(err)
-        }
+  //     }
+  //   }
 
-      }else if(this.state.files[i].type == "image/png"){
+  //   this.findImgNameRelatedToBin();
+  //   this.imgFilesToPhotosRelatedToBin();
+  //   console.log(this.state.photosRelatedToBin)
+  // }
 
-        if(this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]] != undefined){
-          this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]].push(this.state.files[i]);
-        }else{
-          this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]] =[];
-          this.state.imgFiles[this.state.files[i].webkitRelativePath.split('/')[1]].push(this.state.files[i]);
-        }
+  // componentDidMount(){
 
-      }
-    }
+  //   const photoDir = document.getElementById("photoDir");
 
-    this.findImgNameRelatedToBin();
-    this.imgFilesToPhotosRelatedToBin();
-    console.log(this.state.photosRelatedToBin)
-  }
-
-  componentDidMount(){
-
-    const photoDir = document.getElementById("photoDir");
-
-    photoDir.addEventListener('change',(e)=>{
-      this.setState({files: photoDir.files}); 
-      this.controller();
+  //   photoDir.addEventListener('change',(e)=>{
+  //     this.setState({files: photoDir.files}); 
+  //     this.controller();
       
-    })
+  //   })
 
+  // }
+  componentWillUpdate(nextProps, nextState){
+    console.log(nextProps)
   }
 
   render(){
     return (
       <div id="photo">
-          <input type="file" id="photoDir" webkitdirectory="true"/>
       </div>
     )
   }
