04092422de46a1e8be47e9df66a3c8ad744e95db
ydh0520 <ydh2338@naver.com>
2019-08-02 17:24:16 +0900

    cloudpoint

--- src/components/cloudpoint/cloudPoint.js
+++ src/components/cloudpoint/cloudPoint.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
+import ThreeEntryPoint from './threeEntryPoint'
 
 class CloudPoint extends Component {
+  state={
 
+  }
 
   render(){
     return (
       <div id="cloudpoint">
-          cp
+        <div id="tool-box">
+        </div>
+        <ThreeEntryPoint rawData={this.props.binFiles[this.props.index]}></ThreeEntryPoint>  
       </div>
     )
   }
