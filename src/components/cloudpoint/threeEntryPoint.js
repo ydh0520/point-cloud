71667d23190e3d38575efa7d749088b3c99eaa65
ydh0520 <ydh2338@naver.com>
2019-08-05 16:26:05 +0900

    three

--- src/components/cloudpoint/threeEntryPoint.js
+++ src/components/cloudpoint/threeEntryPoint.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import * as THREE from "three"
 import THREECamera from "./threeCamera"
 import THREECloudPoint from "./threeCloudPoint"
@@ -8,46 +8,33 @@ const renderer = new THREE.WebGLRenderer({
   alpha:true
 })
 const camera = THREECamera.create2DCamera(500)
-let scene  = new THREE.Scene();
+let scene  = new THREE.Scene()
 
 class threeEntryPoint extends Component {
   state={
+    cameraState:0,
+    camera2d:'',
+    camera3d:''
   }
 
-  // componentDidMount(){
-    
-  //   const rootDom = document.getElementById("cp-canvas");
-
-  //   rootDom.appendChild(renderer.domElement)
-  //   scene.background=new THREE.Color(0x111111);
-  // }
-
-  // componentWillReceiveProps(nextProps,nextState){
-
-  //   if(typeof nextProps['rawData']!=='undefined'){
-  //     scene  = new THREE.Scene();
-
-  //     const light = new THREE.AmbientLight(0x505050,100)
-  //     scene.add(light)
-
-  //     scene.add(THREECloudPoint.createCloudpoint(nextProps['rawData']))
-  //     console.log(camera)
-  //     console.log(scene)
-  //     console.log(renderer)
-  //     renderer.render(scene,camera)
-
-  //   }
-  // }
-
-  componentWillUpdate(nextProps, nextState){
-    console.log(nextProps)
+  componentDidMount(){
+    const rootDom = document.getElementById("cp-canvas")
+    renderer.setSize(500,500)
+    rootDom.appendChild(renderer.domElement)
+    scene.background=new THREE.Color(0x111111)
   }
 
-  componentDidUpdate(prevProps, prevState){
-    console.log(prevProps)
+  componentWillUpdate(nextProps){
+    scene=new THREE.Scene()
+
+    scene.background=new THREE.Color(0x111111)
+    scene.add(new THREE.AmbientLight(0x505050,100))
+
+    scene.add(THREECloudPoint.createCloudpoint(nextProps['rawData']))
   }
 
   render(){
+    renderer.render(scene,camera)
     return (
         <div id ="cp-canvas"></div>
     )
