e9c674aa3d1cf1e9a84312ab9629d15671d92bc4
ydh0520 <ydh2338@naver.com>
2019-08-06 16:35:23 +0900

    cameraHandler

--- src/components/cloudpoint/threeEntryPoint.js
+++ src/components/cloudpoint/threeEntryPoint.js
@@ -3,28 +3,65 @@ import * as THREE from "three"
 import THREECamera from "./threeCamera"
 import THREECloudPoint from "./threeCloudPoint"
 
-
-const renderer = new THREE.WebGLRenderer({
-  alpha:true
-})
-const camera = THREECamera.create2DCamera(500)
 let scene  = new THREE.Scene()
 
 class threeEntryPoint extends Component {
   state={
+    renderer:'',
+    camera:'',
     cameraState:0,
-    camera2d:'',
-    camera3d:''
+    scene:''
+  }
+
+  constructor(props){
+    super(props);
+
+    const renderer = new THREE.WebGLRenderer({
+      alpha:true
+    })
+
+    const camera = []
+
+    camera.push(THREECamera.create2DCamera(500))
+    camera.push(THREECamera.create3DCamera())
+
+    this.state={
+      renderer:renderer,
+      camera:camera,
+      cameraState:0
+    }
   }
 
   componentDidMount(){
     const rootDom = document.getElementById("cp-canvas")
-    renderer.setSize(500,500)
-    rootDom.appendChild(renderer.domElement)
+
+    let size = window.innerHeight;
+    if(window.innerWidth<size){
+      size=window.innerWidth;
+    }
+
+    this.state.renderer.setSize(size,size) 
+    rootDom.appendChild(this.state.renderer.domElement)
     scene.background=new THREE.Color(0x111111)
+    
+    this.state.renderer.render(scene,this.state.camera[this.state.cameraState]) 
+
+    window.addEventListener('resize',this.handleResize)
+    this.state.renderer.domElement.addEventListener('mousedown',this.handleMousedown)
+    this.state.renderer.domElement.addEventListener('mouseup',this.handleMouseup)
   }
 
-  componentWillUpdate(nextProps){
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      cameraState:nextProps.cameraState
+    })
+  }
+
+  shouldComponentUpdate(nextProps,nextState){
+    return true
+  }
+
+  componentWillUpdate(nextProps,nextState){
     scene=new THREE.Scene()
 
     scene.background=new THREE.Color(0x111111)
@@ -33,12 +70,57 @@ class threeEntryPoint extends Component {
     scene.add(THREECloudPoint.createCloudpoint(nextProps['rawData']))
   }
 
+  componentDidUpdate(prevProps, prevState){
+
+  }
+
   render(){
-    renderer.render(scene,camera)
+    this.state.renderer.render(scene,this.state.camera[this.state.cameraState])
     return (
         <div id ="cp-canvas"></div>
     )
   }
+
+  handleResize=(e)=>{
+    let size = window.innerHeight;
+    if(window.innerWidth<size){
+      size=window.innerWidth;
+    }
+    this.state.renderer.setSize(size,size)
+    this.state.renderer.render(scene,this.state.camera[this.state.cameraState]) 
+  }
+
+  handleMouseup=(e)=>{
+    if(this.state.cameraState===0){
+
+    }else if(this.state.cameraState===1){
+      this.state.renderer.domElement.removeEventListener('mousemove',this.handleMousemove3D)
+    }
+  }
+
+  handleMousedown=(e)=>{
+    if(this.state.cameraState===0){
+
+    }else if(this.state.cameraState===1){
+      this.state.renderer.domElement.addEventListener('mousemove',this.handleMousemove3D)
+    }
+  }
+
+  
+  handleMousemove3D=(e)=>{
+    scene.rotation.z+=e.movementX*0.01
+    scene.rotation.x+=e.movementY*0.01
+
+    this.state.renderer.render(scene,this.state.camera[this.state.cameraState])
+  }
+  
+  handleCreate=(e)=>{
+    scene.rotation.z+=e.movementX*0.01
+    scene.rotation.x+=e.movementY*0.01
+
+    this.state.renderer.render(scene,this.state.camera[this.state.cameraState])
+  }
+
 }
 
 export default threeEntryPoint
--- src/components/photo/photo.js
+++ src/components/photo/photo.js
@@ -3,11 +3,11 @@ const async = require("async")
 class Photo extends Component {
   
     componentDidUpdate(prevProps,prevState){
-      console.log(this.props.binFilesNames)
-      console.log(this.props.index)
-      console.log(this.props.binFilesNames[this.props.index].substring(0,this.props.binFilesNames[this.props.index].length-4))
-      console.log(this.props.photosRelatedToBin)
-      console.log(this.props.photosRelatedToBin[this.props.binFilesNames[this.props.index].substring(0,this.props.binFilesNames[this.props.index].length-4)])
+    //   console.log(this.props.binFilesNames)
+    //   console.log(this.props.index)
+    //   console.log(this.props.binFilesNames[this.props.index].substring(0,this.props.binFilesNames[this.props.index].length-4))
+    //   console.log(this.props.photosRelatedToBin)
+    //   console.log(this.props.photosRelatedToBin[this.props.binFilesNames[this.props.index].substring(0,this.props.binFilesNames[this.props.index].length-4)])
     }
 
     render(){
