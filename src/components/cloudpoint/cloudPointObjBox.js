5783a9764d3cb1d97cad84fcabe5d8c759ae09eb
ydh0520 <ydh2338@naver.com>
2019-08-20 17:42:01 +0900

    objbox style and callback event

--- src/components/cloudpoint/cloudPointObjBox.js
+++ src/components/cloudpoint/cloudPointObjBox.js
@@ -3,8 +3,13 @@ import './cloudPointObjBox.css'
 class cloudPointObjBox extends Component {
     render(){
         const objBoxList = this.props.obj.map(
-            (objPa) => (<li key={objPa.uuid} onClick={(uuid)=>this.selectObj(objPa.uuid)}>
-                <div  className="objBoxes" style={{background: "rgb("+objPa.material.color["r"]*255+","+objPa.material.color["g"]*255+","+objPa.material.color["b"]*255+")"}}>{objPa.name}</div>
+            (objPa) => (
+                <li key={objPa.uuid} onClick={()=>this.selectObj(objPa.uuid)} style={{background: "rgb("+objPa.material.color["r"]*255+","+objPa.material.color["g"]*255+","+objPa.material.color["b"]*255+",0.5)"}}>
+                    <div  className="objBoxes">
+                        {objPa.name}
+                        <i className="xi-trash-o" onClick={()=>this.deleteObj(objPa.uuid)}></i>
+                        <i className="xi-caps" onClick={()=>this.renameObj(objPa.uuid)}></i>
+                    </div>
                 </li>)
         );
          return (
@@ -14,15 +19,29 @@ class cloudPointObjBox extends Component {
         )
     }
     
-    selectObj(uuid){
-        let index;
+    uuidtoIdex=(uuid)=>{
+        let index
         for(var i=0;i<this.props.obj.length;i++){
             if(this.props.obj[i].uuid===uuid){
                 index=i
             }
         }
-    
-        this.props.callbackControlObjBox(1,index)
+        return index;
+    }
+
+    selectObj=(uuid)=>{
+        let index=this.uuidtoIdex(uuid);
+        this.props.callbackControlObjBox(0,index,null)
+    }
+
+    deleteObj=(uuid)=>{
+        let index=this.uuidtoIdex(uuid);
+        this.props.callbackControlObjBox(1,index,null)
+    }
+
+    renameObj=(uuid,value)=>{
+        let index=this.uuidtoIdex(uuid);
+        this.props.callbackControlObjBox(2,index,"rename")
     }
 }
 
